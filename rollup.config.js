9ae4bfddc1c7288c032da8d3e60cd198988f8265
Magnus Claesson <magnus.claesson@gmail.com>
2026-07-04 15:32:56 +0200

    feat: add @rollup/plugin-terser for code minification and update rollup configuration to include terser plugin for improved build optimization

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import alias from '@rollup/plugin-alias'
 import nodeResolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -19,5 +20,13 @@ export default {
     }),
     nodeResolve(),
     typescript(),
+    terser({
+      mangle: {
+        safari10: true, // Fixar eventuella quirks i äldre WebKit/Home Assistant-appar
+      },
+      format: {
+        comments: false, // Tar bort alla kommentarer helt
+      },
+    }),
   ],
 }
